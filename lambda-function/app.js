777775d088d5ef134ebcbfea451cec3962d8013f
vcalatayud <lacuentadevictor@gmail.com>
2023-05-07 12:15:59 +0200

    removed lambda components

--- lambda-function/app.js
+++
@@ -1,8 +0,0 @@
-const AWS = require('aws-sdk');
-const Calculator = require('./domain/calculator');
-
-exports.handler = async (event) => {
-    const calculator = Calculator();
-    let result = await calculator.sum(event.a, event.b);
-    return result
-};
